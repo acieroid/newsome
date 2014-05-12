59a61ba0bf75e5885d974e90804cefe72f30c9c5
Quentin Stievenart <quentin.stievenart@gmail.com>
2014-05-12 17:18:43 +0200

    Add nginx to master jail

--- install/postinst-setup.sh
+++ install/postinst-setup.sh
@@ -28,10 +28,25 @@ echo 'cloned_interfaces="lo1"' >> /etc/rc.conf
 echo 'ifconfig_lo1="inet 172.16.0.1 netmask 255.255.255.0"' >> /etc/rc.conf
 ifconfig lo1 create inet 172.16.0.1 netmask 255.255.255.0
 
-# TODO: adapt the pf.conf with the current configuration
+# TODO: adapt the pf.conf with the machine configuration (interface, ip, etc.)
 cp pf.conf /etc/pf.conf
 pfctl -F all -f /etc/pf.conf
 
 # master jail
 ezjail-admin create -f master master "lo1|172.16.0.1"
 ezjail-admin start master
+ezjail-admin console -e "pkg" master # answer y
+
+# unbound in master: TODO
+#ezjail-admin console -e "fetch ftp://ftp.internic.net/domain/named.cache" master
+#ezjail-admin console -e "mv named.cache /etc/unbound/root.hints" master
+
+# nginx in master
+# TODO: ssl (one per subdomain)
+# TODO: factor ezjail-admin console -e x jail
+ezjail-admin console -e "pkg -y install nginx" master
+cp master-nginx.conf /usr/jails/master/usr/local/etc/nginx/nginx.conf
+mkdir -p /usr/jails/master/usr/local/www/master/
+cp master-index.html /usr/jails/master/usr/local/www/master/index.html
+echo 'nginx_enable="YES"' >> /usr/jails/master/etc/rc.conf
+ezjail-admin console -e "service nginx start" master
