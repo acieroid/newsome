67bf88e5b5f4c18f0a3d2376a67a024b34026eff
Quentin Stievenart <quentin.stievenart@gmail.com>
2014-05-22 23:01:00 +0200

    Add script to update sh scripts in jails

---
+++ service-utils/service-utils-update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -o errexit
+set -o nounset
+
+# Copy service-utils files to the bin directory of every user's bin/ directory
+# in the given jail. Expects the jail as argument, and the service-utils files
+# to be present in /root/bin.
+
+if [ "$#" -ne 1 ]; then
+    echo "$0: expecting jail in argument"
+    exit 1
+fi
+
+JAIL="$1"
+
+if [ ! -d "/usr/jails/$JAIL" ]; then
+    echo "Cannot update service-util in jail '$JAIL', the jail doesn't exist."
+    exit 1
+fi
+
+find "/usr/jails/$JAIL/home" -maxdepth 1 -exec \
+    cp -f /root/bin/service-jail-action.sh "{}/bin"
